67a409f313fc23f8d818843354ea35f2f0ce4168
HyeonSeok_Cha <hs960829@gmail.com>
2020-10-16 23:44:42 +0900

    add selectionTracker.

--- app/src/main/java/com/chs/readytonote/ui/MainActivity.kt
+++ app/src/main/java/com/chs/readytonote/ui/MainActivity.kt
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.selection.SelectionPredicates
@@ -19,15 +21,17 @@ import com.chs.readytonote.adapter.NoteDetailsLookup
 import com.chs.readytonote.entities.Note
 import com.chs.readytonote.viewmodel.MainViewModel
 import com.chs.readytonote.viewmodel.MainViewModelFactory
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     companion object {
-        private const val REQUST_CODE_SHOW_NOTE = 1
+        private const val REQUEST_CODE_SHOW_NOTE = 1
     }
 
     private lateinit var notesAdapter: NoteAdapter
     private lateinit var viewModel: MainViewModel
+    private lateinit var noteSelectionTracker: SelectionTracker<Long>
     private var noteClickPosition = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -44,11 +48,12 @@ class MainActivity : AppCompatActivity() {
     private fun initClick() {
         imgAddNoteMain.setOnClickListener {
             startActivityForResult(Intent(this,
-                CreateNoteActivity::class.java),REQUST_CODE_SHOW_NOTE)
+                CreateNoteActivity::class.java),REQUEST_CODE_SHOW_NOTE)
         }
     }
     private fun initView(){
-        bottomAppBar.replaceMenu(R.menu.main_note)
+        bottomAppBar.replaceMenu(R.menu.create_note)
+
         bottomAppBar.setOnMenuItemClickListener {
             viewModel.allDelete()
             getNote()
@@ -63,12 +68,13 @@ class MainActivity : AppCompatActivity() {
                 val intent = Intent(this@MainActivity, CreateNoteActivity::class.java)
                 intent.putExtra("isViewOrUpdate", true)
                 intent.putExtra("note", note)
-                startActivityForResult(intent,REQUST_CODE_SHOW_NOTE)
+                startActivityForResult(intent,REQUEST_CODE_SHOW_NOTE)
             })
             this.layoutManager = StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL)
             this.adapter = notesAdapter
+            getNote()
             // still selectionTracker Problem..
-            val noteSelectionTracker = SelectionTracker.Builder<Long>(
+            noteSelectionTracker = SelectionTracker.Builder<Long>(
                 "note-content",
                 Rv_notes,
                 StableIdKeyProvider(Rv_notes),
@@ -78,8 +84,17 @@ class MainActivity : AppCompatActivity() {
                 SelectionPredicates.createSelectAnything()
             ).build()
             notesAdapter.setTracker(noteSelectionTracker)
+
+            noteSelectionTracker.addObserver(
+                object : SelectionTracker.SelectionObserver<Long>() {
+                    override fun onSelectionChanged() {
+                        super.onSelectionChanged()
+                        bottomAppBar.replaceMenu(R.menu.main_note)
+                        Log.d("SelectionSize",noteSelectionTracker.selection.size().toString())
+                    }
+                }
+            )
         }
-        getNote()
     }
 
     private fun getNote() {
@@ -104,5 +119,13 @@ class MainActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         getNote()
+    }
+
+    override fun onBackPressed() {
+        if(::noteSelectionTracker.isLateinit &&
+                noteSelectionTracker.hasSelection()) {
+            
+        }
+        super.onBackPressed()
     }
 }
