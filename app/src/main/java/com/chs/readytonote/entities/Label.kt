5cc862d0f4b473e62cb4a85e7df237ed917fd070
HeonSeokCha <hs960829@gmail.com>
2020-12-01 23:45:55 +0900

    add Label table.

--- app/src/main/java/com/chs/readytonote/entities/Label.kt
+++ app/src/main/java/com/chs/readytonote/entities/Label.kt
@@ -1,10 +1,45 @@
 package com.chs.readytonote.entities
 
+import android.os.Parcel
+import android.os.Parcelable
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(tableName = "label")
 data class Label(
     @ColumnInfo(name = "name")
-    val name: String
-)
+    val name: String?,
+    @ColumnInfo(name = "checked")
+    val checked: Boolean = false,
+):Parcelable {
+    @PrimaryKey(autoGenerate = true)
+    var id: Int = 0
+
+    constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        parcel.readByte() != 0.toByte()
+    ) {
+        id = parcel.readInt()
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(name)
+        parcel.writeByte(if (checked) 1 else 0)
+        parcel.writeInt(id)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Label> {
+        override fun createFromParcel(parcel: Parcel): Label {
+            return Label(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Label?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
